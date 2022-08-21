3f40e09e7a77a956e934b10955c33d149085818c
kruti2107 <chodwadiakruti@gmail.com>
2022-08-21 14:16:15 +0200

    Design login and registration page

--- My_Notes/views.py
+++ My_Notes/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def login(request):
+    return render(request, 'login.html')
+
 def home(request):
     return render(request, 'home.html')
+
+def register(request):
+    return render(request, 'registration.html')
--- Notes_Application/settings.py
+++ Notes_Application/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'My_Notes.apps.MyNotesConfig',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
@@ -117,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static/"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
